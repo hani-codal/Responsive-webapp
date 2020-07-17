9a688fee89877dd01606872291f93c6f1abef565
hani-codal <hjoshi@codal.com>
2020-07-17 10:36:30 +0530

    commit for images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Options } from 'ng5-slider';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Responsive-Design';
+  options: Options = {
+    floor: 1000,
+    ceil: 10000
+  };
+  isTour=true;
+  isHotel=false;
+  isFlight=false;
+  onTour(){
+    this.isTour=true;
+    this.isHotel=false;
+    this.isFlight=false;
+  }
+
+  onHotel(){
+    this.isHotel=true;
+    this.isFlight=false;
+    this.isTour=false
+  }
+  onFlight(){
+    this.isHotel=false  ;
+    this.isFlight=true;
+    this.isTour=false
+  }
+  onDestination(){
+    window.scrollTo({
+
+      behavior: 'smooth'
+    });
+  }
+
 }
