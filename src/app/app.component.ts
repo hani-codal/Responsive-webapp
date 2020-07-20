1dc46efd48149505ee67c24ccba3a504b8fbb5cf
hani-codal <hjoshi@codal.com>
2020-07-20 20:18:41 +0530

    commit for countdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,4 +38,33 @@ export class AppComponent {
     });
   }
 
+
+
+// Update the count down every 1 second
+ x = setInterval(function() {
+ var countDownDate = (new Date("Jan 5, 2021 15:37:25").getTime());
+
+  // Get today's date and time
+  var now =( new Date().getTime());
+  console.log(now,"now");
+  // Find the distance between now and the count down date
+  var distance = (countDownDate - now);
+  console.log(countDownDate,"dis")
+  // Time calculations for days, hours, minutes and seconds
+  var days =Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  console.log(days)
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d : " + hours + "h : "
+  + minutes + "m : " + seconds + "s ";
+
+  // If the count down is over, write some text
+  if (distance < 0) {
+    clearInterval(this.x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
 }
